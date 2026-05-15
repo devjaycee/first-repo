4157f4456c6a08592be10cb161bc09cae2a91477
devjaycee <jcperdon54@gmail.com>
2026-05-15 18:58:30 +0800

    add greeting message

--- hello.py
+++ hello.py
@@ -1,11 +1,1 @@
-import random
-
-secret = random.randint(1, 10)
-guess = int(input("Guess a number between 1 and 10: "))
-
-if guess == secret:
-    print("You got it!")
-elif guess < secret:
-    print("Too low!")
-else:
-    print("Too high!")
+print("Welcome to our project!")
